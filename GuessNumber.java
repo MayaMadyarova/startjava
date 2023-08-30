4f5b1716532747ac7e2bbe50b89611681ce33258
MayaMadyarova <mayamadyarova@mail.ru>
2023-08-30 13:25:30 +0300

    исправлены замечания Максима

--- GuessNumber.java
+++ GuessNumber.java
@@ -5,15 +5,15 @@ public class GuessNumber {
         int min = 0;
         System.out.println("Please, input number which is less then 100 and bigger or equal 1");
         int inputNum = 45;
-        while (inputNum != secretNum && inputNum <= max && inputNum > min) {
+        while (inputNum != secretNum && inputNum > min && inputNum <= max) {
             if(inputNum > secretNum) {
                 System.out.println("Number " + inputNum + " is bigger than what the computer made up");
-                inputNum --;
+                inputNum--;
             } else {
                 System.out.println("Number " + inputNum + " is less than what the computer made up");
-                inputNum ++;
+                inputNum++;
             }
         }
-    System.out.println("Number " + inputNum + " is what the computer made up. You win!");
+        System.out.println("Number " + inputNum + " is what the computer made up. You win!");
     }
 }
