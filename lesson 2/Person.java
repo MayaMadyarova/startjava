85c69895cfc97c0078af793569684a95094d3fd2
MayaMadyarova <mayamadyarova@mail.ru>
2023-09-01 16:21:19 +0300

    создан класс Person

---
+++ lesson 2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    
+    String sex = "male";
+    String name = "Tim";
+    float height  = 1.82f;
+    float weight = 70.5f;
+    int age = 38;
+
+    public void go() {
+        System.out.println("going");
+    }
+
+    public void sit() {
+        System.out.println("sitting");
+    }
+
+    public void run() {
+        System.out.println("running");
+    }
+
+    public void speak() {
+        System.out.println("speaking");
+    }
+
+    public void learnJava() {
+        System.out.println("learning Java");
+    }
+
+}
