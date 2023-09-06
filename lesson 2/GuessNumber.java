d6369618fad8c982ba3eff935f8de61feee46896
MayaMadyarova <mayamadyarova@mail.ru>
2023-09-06 17:39:31 +0300

    исправлены замечания Максима

--- lesson 2/GuessNumber.java
+++ lesson 2/GuessNumber.java
@@ -1,39 +1,43 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-    Player playerOne;
-    Player playerTwo;
+    Scanner console = new Scanner(System.in);
+    private Player player1;
+    private Player player2;
 
-    public GuessNumber(Player playerOne, Player playerTwo) {
-        this.playerOne = playerOne;
-        this.playerTwo = playerTwo;
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
-    Scanner s = new Scanner(System.in);
-    int secretNumber = (int)(1 + Math.random() * 100);
-
-    public void game(Player playerOne, Player playerTwo) {
-            do {
-                System.out.println("The first player " + playerOne.getName() + " input your number");
-                playerOne.setNumber(s.nextInt());
-                int number1 = playerOne.getNumber();
-                guessNumber(number1, secretNumber);
-                if(number1 != secretNumber) {
-                    System.out.println("The second player " + playerTwo.getName() + " input your number");
-                    playerTwo.setNumber(s.nextInt());
-                    int number2 = playerTwo.getNumber();
-                guessNumber(number2, secretNumber);
-                }
-            } while (playerOne.getNumber() != secretNumber && playerTwo.getNumber() != secretNumber);
+    public void playGame() {
+        int secretNumber = (int)(1 + Math.random() * 100);
+        do {
+            boolean isEqual = guessNumber(inputNumber(player1), secretNumber);
+            if(!isEqual) {
+                guessNumber(inputNumber(player2), secretNumber);
+            }
+        } while (player1.getNumber() != secretNumber && player2.getNumber() != secretNumber);
     }
 
-    public void guessNumber(int number, int secretNumber) {
+    private boolean guessNumber(int number, int secretNumber) {
+        boolean isEqual = true;
         if(number > secretNumber) {
             System.out.println("Number " + number + " is bigger than what the computer made up");
+            return false;
         } else if (number < secretNumber) {
             System.out.println("Number " + number + " is less than what the computer made up");
+            return false;
         } else {
             System.out.println("Number " + number + " is what the computer made up. You win!");
+            return true;
         }
+    }
+
+    private int inputNumber(Player player) {
+        System.out.println("Player " + player.getName() + " input your number");
+        player.setNumber(console.nextInt());
+        int number = player.getNumber();
+        return number;
     }
 }
--- lesson 2/GuessNumberTest.java
+++ lesson 2/GuessNumberTest.java
@@ -4,18 +4,18 @@ public class GuessNumberTest {
 
     public static void main(String[] args) {
         String answer;
-        Scanner s = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         System.out.println("Input the name of the first player");
-        Player playerOne = new Player(s.nextLine());
+        Player player1 = new Player(console.nextLine());
         System.out.println("Input the name of the second player");
-        Player playerTwo = new Player(s.nextLine());
-        GuessNumber guessNumber = new GuessNumber(playerOne, playerTwo);
+        Player player2 = new Player(console.nextLine());
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
 
         do {
-            guessNumber.game(playerOne, playerTwo);
+            guessNumber.playGame();
             do {
                 System.out.println("Do You want to continue?[yes/no]");
-                answer = s.nextLine();
+                answer = console.nextLine();
             } while(!answer.equals("no") && !answer.equals("yes"));
         } while(answer.equals("yes"));
         System.out.println("The game is over");
