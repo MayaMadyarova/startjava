d5f76e012e8dd63a9f22b61d7153cc895d7f696c
MayaMadyarova <mayamadyarova@mail.ru>
2023-09-08 17:41:46 +0300

    несены правки

--- lesson 2/GuessNumber.java
+++ lesson 2/GuessNumber.java
@@ -12,29 +12,27 @@ public class GuessNumber {
 
     public void play() {
         int secretNumber = (int) (1 + Math.random() * 100);
-        while(!guess(inputNumber(player1), secretNumber, player1) && !guess(inputNumber(player2), secretNumber, player2)) {
-            if(!guess(inputNumber(player1), secretNumber, player1)) {
-                guess(inputNumber(player2), secretNumber, player2);
-            } else 
-            break;
+        while(true) {
+            if(!guess(secretNumber, player1)) {
+                if(guess(secretNumber, player2)) {
+                    break;
+                }
+            } else {
+                break;
+            }
         }
     }
 
-    private int inputNumber(Player player) {
+     private boolean guess(int secretNumber, Player player) {
         System.out.println("Player " + player.getName() + " input your number");
         player.setNumber(console.nextInt());
-        return player.getNumber();
-    }
-
-     private boolean guess(int number, int secretNumber, Player player) {
+        int number = player.getNumber();
         if(number == secretNumber) {
             System.out.println("Number " + number + " is what the computer made up." + player.getName() + " you win!");
             return true;
-        }
-        if(number > secretNumber) {
+        } else if(number > secretNumber) {
             System.out.println("Number " + number + " is bigger than what the computer made up");
-        }
-        if (number < secretNumber) {
+        } else if(number < secretNumber) {
             System.out.println("Number " + number + " is less than what the computer made up");
         }
         return false;
