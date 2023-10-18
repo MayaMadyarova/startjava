68c2eb378e5368aacc9c703cb37a3bcf6e5f5b73
Maya Madyarova <maiya_mb4@mail.ru>
2023-10-18 19:57:11 +0300

    внесены правки в калькулятор

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,67 +1,29 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    private static int a;
-    private static int b;
-    private static char sign;
-    private static double result;
+    public static int a;
+    public static int b;
+    public static char sign;
+    public static double result;
+    String[] elements = new String[3];
 
     Calculator(String mathExpression) {
         String[] elements = mathExpression.split(" ");
         a = Integer.parseInt(elements[0]);
-        sign = elements[1].charAt(0);
         b = Integer.parseInt(elements[2]);
-    }
-
-
-    static void check() {
-        try {
-            boolean check1 = (a > 0 && b > 0);
-        }
-        catch (Exception e) {
-            RuntimeException exception;
-            System.out.println("Numbers must be positive");
-        }
-        try {
-            boolean check2 = (a % 1 == 0 && b % 1 == 0);
-        }
-        catch (Exception e) {
-            RuntimeException exception;
-            System.out.println("Numbers must be integers");
-        }
-
+        sign = elements[1].charAt(0);
     }
 
     static double calculate() {
         result =
         switch(sign) {
-            case '+' : {
-                yield a + b;
-            }
-        case '-' : {
-                yield a - b;
-            }
-            case '*' : {
-                yield a * b;
-            }
-            case '/' : {
-                if(b == 0) {
-                    System.out.println("You can't devide by zero!");
-                    yield Double.MIN_VALUE;
-                } else {
-                    yield (double) a / b;
-                }
-            }
-            case '%' : {
-                yield a % b;
-            }
-            case '^' : {
-                yield Math.pow((double) a, b);
-            }
-            default : {
-                System.out.println("Error: sign " + sign + " is not supported.");
-                yield Double.MIN_VALUE;
-            }
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> a / b;
+            case '%' -> a % b;
+            case '^' -> Math.pow((double) a, b);
+            default -> throw new IllegalStateException();
         };
         return result;
     }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,21 +7,46 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String answer = "";
+        double result =0;
 
         do {
             System.out.println("Input the mathematical expression in the format: 2 ^ 10");
             String mathExpression = scanner.nextLine();
-            double result = Calculator.calculate();
-            outputResult(result, mathExpression);
-            do {
+            try {
+                new Calculator(mathExpression);
+                if(Calculator.a < 0 || Calculator.b < 0) {
+                    try {
+                        throw new RuntimeException();
+                    }
+                    catch (RuntimeException e) {
+                        System.out.println("Numbers must be positive!");
+                    }
+                }
+                result = Calculator.calculate();
+                outputResult(result, mathExpression);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Numbers must be integers!");
+            }
+            catch (ArithmeticException e) {
+                System.out.println("You can't devide by zero!");
+            }
+            catch (IllegalStateException e) {
+                System.out.println("Sign is not supported!");
+            }
+
+
+            System.out.println("Do you want to continue calculation? [yes/no]: ");
+            answer = scanner.nextLine();
+            if (!answer.equals("no") && !answer.equals("yes")) {
                 System.out.println("Do you want to continue calculation? [yes/no]: ");
                 answer = scanner.nextLine();
-            } while (!answer.equals("no") && !answer.equals("yes"));
+            }
         } while (answer.equals("yes"));
     }
 
     private static void outputResult(double result, String mathExpression) {
-        System.out.println(result != Double.MIN_VALUE ? mathExpression + " = "
+        System.out.println(result > 0 ? mathExpression + " = "
                 + new DecimalFormat("#.###").format(result) : "");
     }
 }
