84d71cedb82053adcbf0ffd8880ef06a075ffd91
Maya Madyarova <maiya_mb4@mail.ru>
2023-10-23 14:38:29 +0300

    внесены правки в калькулятор

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,28 +2,33 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    static double calculate(String mathExpression) throws RuntimeException {
+    static double calculate(String mathExpression) {
         String[] elements = mathExpression.split(" ");
         if(elements.length != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("The length of mathematical expression is not correct!");
         }
-        int a = Integer.parseInt(elements[0]);
-        char sign = elements[1].charAt(0);
-        int b = Integer.parseInt(elements[2]);
-        if(a % 1 != 0 || b % 1 != 0) {
-            throw new NumberFormatException();
+        int a;
+        int b;
+        char sign;
+        try {
+            a = Integer.parseInt(elements[0]);
+            sign = elements[1].charAt(0);
+            b = Integer.parseInt(elements[2]);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Numbers must be integers!");
         }
-        if(a < 0 || b < 0) {
-            throw new RuntimeException();
+        if(!(a > 0) || !(b > 0)) {
+            throw new RuntimeException("Numbers must be positive!");
         }
         return switch (sign) {
             case '+' -> a + b;
             case '-' -> a - b;
             case '*' -> a * b;
-            case '/' -> a / b;
+            case '/' -> {if(b == 0) {throw new ArithmeticException("You can't devide by zero!");}
+                yield (double) a / (double) b;}
             case '%' -> a % b;
             case '^' -> Math.pow((double) a, b);
-            default -> throw new IllegalStateException("");
+            default -> throw new IllegalStateException("Sign is not supported!");
         };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -16,20 +16,12 @@ public class CalculatorTest {
                 try {
                     double result = Calculator.calculate(mathExpression);
                     System.out.println(mathExpression + " = " + new DecimalFormat("#.###").format(result));
-                } catch (NumberFormatException e) {
-                    System.out.println("Numbers must be integers!");
-                } catch (IllegalArgumentException e) {
-                    System.out.println("The length of mathematical expression is not correct!");
-                } catch (ArithmeticException e) {
-                    System.out.println("You can't devide by zero!");
-                } catch (IllegalStateException e) {
-                    System.out.println("Sign is not supported!");
                 } catch (RuntimeException e) {
-                    System.out.println("Numbers must be positive!");
+                    System.out.println(e.getMessage());
                 }
             }
-            System.out.println("Do you want to continue calculation? [yes/no]: ");
-            answer = scanner.nextLine();
+        System.out.println("Do you want to continue calculation? [yes/no]: ");
+        answer = scanner.nextLine();
         } while (!"no".equals(answer));
     }
 }
