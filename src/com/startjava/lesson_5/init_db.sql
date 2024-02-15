98a6b635aeebef95fe4c8524149d69a6a5f60772
Maya Madyarova <maiya_mb4@mail.ru>
2024-02-16 02:08:07 +0300

    добавлены файлы к 5 уроку

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill)
+   VALUES 
+        ('Cherno Alpha', 1, 85.34, 2.412, 'Destroyed', 'Russia', '2015.01.01', 7),
+        ('Gipsy Danger', 3, 79.25, 1.980, 'Destroyed', 'United States of America', '2017.07.10', 9),
+        ('Striker Eureka', 5, 76.2, 2.087, 'Destroyed', 'Australia', '2019.11.02', 11),
+        ('Gipsy Avenger', 6, 81.77, 2.120, 'Active', 'Germany', '2034.01.12', 1),
+        ('Coyote Tango', 1, 85.34, 2.326, 'Active','Japan', '2015.12.30',2),
+        ('Crimson Typhoon', 4, 76.2, 6.122, 'Destroyed', 'China','2018.08.22', 8),
+        ('Brawler Yukon', 1, 64.28, 4.586, 'Active', 'United States of America', '2015.01.30', 2),
+        ('Marauder', 2, 80.95, 3.081, 'Destroyed', 'Spain', '2014.05.05', 3),
+        ('Atlas Psycho', 1, 90.21, 5.046, 'Active', 'Italy', '2015.08.20', 7),
+        ('Lucky Seven', 1, 74.28, 1.765, 'Destroyed', 'Canada', '2019.03.15', 5);
